15960823393b7a3f8eeee141ff4784c994485a1c
Richard Husted Jr <richard.husted@gmail.com>
2016-08-24 15:14:55 -0500

    More practice.

--- CrackingTheCodingInterview/General/BinaryTree.cs
+++ CrackingTheCodingInterview/General/BinaryTree.cs
@@ -1,12 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace General
 {
-    class BinaryTree
+    public class BinaryTreeNode<T> where T : IComparable
     {
+        public BinaryTreeNode<T> Left { get; set; }
+
+        public BinaryTreeNode<T> Right { get; set; }
+
+        public T Value { get; set; }
+    }
+
+    public static class BinaryTree
+    {
+        public static BinaryTreeNode<T> CreateFromArray<T>(T[] items) where T : IComparable
+        {
+            if (items == null || items.Length <= 0 ) return null;
+            var node = new BinaryTreeNode<T>() { Value = items[0] };
+            for (var index = 1; index < items.Length; index++)
+                AddNode(items[index], node);
+            return node;
+        }
+
+        public static T Search<T>(BinaryTreeNode<T> node, T value) where T : IComparable
+        {
+            if (node == null) return default(T);
+            var current = node;
+            while(current != null)
+            {
+                var compare = current.Value.CompareTo(value);
+                if (compare == 0)
+                    return value;
+                else if (compare > 0)
+                    current = current.Left;
+                else
+                    current = current.Right;
+            }
+            return default(T);
+        }
+
+        private static void AddNode<T>(T value, BinaryTreeNode<T> node) where T : IComparable
+        {
+            if (node == null) return;
+            if(value.CompareTo(node.Value) < 0)
+            {
+                if (node.Left == null)
+                    node.Left = new BinaryTreeNode<T>() { Value = value };
+                else
+                    AddNode(value, node.Left);
+
+            }
+            else if(value.CompareTo(node.Value) > 0)
+            {
+                if (node.Right == null)
+                    node.Right = new BinaryTreeNode<T>() { Value = value };
+                else
+                    AddNode(value, node.Right);
+            }
+        }
     }
 }
---
+++ CrackingTheCodingInterview/TreesAndGraphs/Exercises.cs
@@ -0,0 +1,20 @@
+﻿using General;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TreesAndGraphs
+{
+    [TestClass]
+    public class Exercises
+    {
+        [TestMethod]
+        public void BinarySearch()
+        {
+            var node = BinaryTree.CreateFromArray(new[] { 50, 30, 20, 40, 70, 60, 80 });
+            Assert.AreEqual(50, BinaryTree.Search(node, 50));
+            Assert.AreEqual(40, BinaryTree.Search(node, 40));
+            Assert.AreEqual(60, BinaryTree.Search(node, 60));
+            Assert.AreEqual(default(int), BinaryTree.Search(node, 90));
+            Assert.AreEqual(default(int), BinaryTree.Search(node, 10));
+        }
+    }
+}
